435fb48416e9e0a9e712bb950a3c95f5e71044e8
kaitabuchi <kaidanieltabuchi@gmail.com>
2024-03-10 17:10:51 -0500

    Work on logging

--- Sunflower-Core/Source/Core/Debug.cpp
+++ Sunflower-Core/Source/Core/Debug.cpp
@@ -1,8 +1,17 @@
 #pragma once
 #include <iostream>
+#include <string>
 namespace Sunflower
 {
 	void Log(const char* text)
+	{
+		std::cout << text;
+	}
+	void Log(float text)
+	{
+		std::cout << text;
+	}
+	void Log(int text)
 	{
 		std::cout << text;
 	}
@@ -10,6 +19,7 @@ namespace Sunflower
 	{
 		std::cout << "Warning: " + text;
 	}
+
 	void LogError(std::string text)
 	{
 		std::cout << "Error: " + text;
