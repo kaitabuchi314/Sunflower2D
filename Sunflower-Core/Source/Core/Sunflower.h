85cdb39efc13c534a6c8677fc35a7e1d861ab384
kaitabuchi <kaidanieltabuchi@gmail.com>
2024-03-10 17:13:43 -0500

    oh wait i did an error

--- Sunflower-Core/Source/Core/Sunflower.h
+++ Sunflower-Core/Source/Core/Sunflower.h
@@ -1,12 +1,11 @@
 #pragma once
 #include <SDL.h>
-
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 #include "GraphicsManager.cpp"
 #include "vec2.h"
 #include "Image.h"
-#include "Debug.h"
+#include "Debug.cpp"
 #include "Keycodes.h"
 #include "GameObject.h"
 #include "Extender.h"
--- Sunflower-Core/Source/Core/vec2.h
+++ Sunflower-Core/Source/Core/vec2.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <string>
-#include "Core/Debug.h"
 
 namespace Sunflower
 {
